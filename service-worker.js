7a78d704ca309175cfa86cf6e3916d22f270eb56
ryo065492-cpu <ryo065492@gmail.com>
2026-04-25 13:03:57 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,79 @@
+const CACHE_NAME = "nap-checker-2026-04-24-231846";
+const BUILD_LABEL = "2026-04-24 23:18";
+const BUILD_ID = "2026-04-24-231846";
+const APP_SHELL_URL = "./";
+const PRECACHE_URLS = [
+  "./",
+  "./index.html",
+  "./nap-checker.html",
+  "./manifest.webmanifest",
+  "./assets/icon.svg",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS)).then(() => self.skipWaiting()),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key)),
+      ),
+    ).then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("message", (event) => {
+  if (event.data?.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(async () => (await caches.match(event.request)) || caches.match(APP_SHELL_URL)),
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type === "opaque") {
+          return response;
+        }
+
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      });
+    }),
+  );
+});
+
+self.addEventListener("messageerror", () => {});
