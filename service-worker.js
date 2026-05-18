0f36b6de4b4fe1d74fb2c972d187a1d269a160f7
ryo065492-cpu <ryo065492@gmail.com>
2026-05-18 14:26:45 +0900

    update nap checker 2026-05-18-1426

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "nap-checker-2026-05-18-121938";
-const BUILD_LABEL = "2026-05-18 12:19";
-const BUILD_ID = "2026-05-18-121938";
+const CACHE_NAME = "nap-checker-2026-05-18-142628";
+const BUILD_LABEL = "2026-05-18 14:26";
+const BUILD_ID = "2026-05-18-142628";
 const APP_SHELL_URL = "./";
 const PRECACHE_URLS = [
   "./",
